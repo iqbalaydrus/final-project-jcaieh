77ae5357ce11e5cc70920e1b0947e8d6060a9b4d
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2025-12-02 20:39:07 +0700

    some more skeletons

--- iqbal/api/main.py
+++ iqbal/api/main.py
@@ -3,22 +3,27 @@ from typing import Optional
 from contextlib import asynccontextmanager
 
 import aiosqlite
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, UploadFile
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 load_dotenv()
 
 db: Optional[aiosqlite.Connection] = None
 API_KEY = os.getenv("API_KEY")
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global db
     db = await aiosqlite.connect("database.db")
     yield
+
+
 app = FastAPI(lifespan=lifespan)
 bearer_scheme = HTTPBearer()
+
 
 def verify_api_key(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
     token = credentials.credentials
@@ -29,6 +34,28 @@ def verify_api_key(credentials: HTTPAuthorizationCredentials = Depends(bearer_sc
         )
     return True
 
-@app.get("/something")
-async def something(_: bool = Depends(verify_api_key)):
+
+@app.post("/upload")
+async def upload(
+    file: UploadFile,
+    _: bool = Depends(verify_api_key),
+):
+    return {"message": "Hello World"}
+
+
+class ChatMessage(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    history: list[ChatMessage]
+    message: ChatMessage
+
+
+@app.post("/chat")
+async def chat(
+    request: ChatRequest,
+    _: bool = Depends(verify_api_key),
+):
     return {"message": "Hello World"}
--- iqbal/web/main.py
+++ iqbal/web/main.py
@@ -1,12 +1,27 @@
+from typing import Any
+
 import streamlit as st
+import requests
 
 DISCORD_CHANNEL_NAME = st.secrets["DISCORD_CHANNEL_NAME"]
 REST_API_BASE_URL = st.secrets["REST_API_BASE_URL"]
 REST_API_KEY = st.secrets["REST_API_KEY"]
 
+
+def api(path: str, data: Any) -> requests.Response:
+    return requests.post(
+        REST_API_BASE_URL + path,
+        headers={
+            "Authorization": "Bearer " + REST_API_KEY,
+            "Content-Type": "application/json",
+        },
+        json=data,
+    )
+
+
 st.title("Final Project")
 st.write(
-    """Upload an image with one or multiple car, bus, or van in it. And we'll output the count of each vechicle type."""
+    """Upload an image with one or multiple car, bus, or van in it. And we'll output the count of each vehicle type."""
 )
 
 for k, v in st.session_state.items():
@@ -15,3 +30,9 @@ for k, v in st.session_state.items():
 if not st.session_state.get("token") or st.session_state.token != DISCORD_CHANNEL_NAME:
     st.text_input("Enter our discord channel name - needed for auth:", key="token")
     st.write(":red[Incorrect channel name]")
+else:
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
